2b5e35c3b50a629885b7912c8ac8711f0de593d0
Ricardo Augusto <contato@ricardoaugusto.com>
2021-03-21 08:11:24 -0300

    Vue + Tailwind
    Signed-off-by: Ricardo Augusto <contato@ricardoaugusto.com>

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="home">
+    <HelloWorld msg="This is the Home" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
